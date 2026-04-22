061436873bf539d3447662ae12810937b90e8737
Rafael Isla <islar@spu.edu>
2026-04-22 11:01:15 -0700

    30

--- enemies.js
+++ enemies.js
@@ -1,5 +1,5 @@
 const enemyTypes = [
-  { speed: 0.3, size: 30, color: "green", points: 10 },
-  { speed: 0.3, size: 30, color: "yellow", points: 20 },
-  { speed: 0.3, size: 30, color: "red", points: 30 }
+  { speed: 0.5, size: 30, color: "green", points: 10 },
+  { speed: 0.5, size: 30, color: "yellow", points: 20 },
+  { speed: 0.5, size: 30, color: "red", points: 30 }
 ];
